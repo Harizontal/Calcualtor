4cab8666cd9ec1a0a96fa8effb83855e8ac17236
Alexey Machihine <alesha.machihine2005@mail.ru>
2024-06-03 22:01:52 +0300

    Revert "Добавьте файлы проекта."
    This reverts commit 68226f3250c9ff211aa5ed606d64b2106465ee2f.

---
+++ FindElements/FindElements.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindElements
+{
+    public class FindElement
+    {
+        public double FindMaxElement(double[] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Массив пуст");
+            }
+
+            double maxElement = double.MinValue;
+            foreach (double element in array)
+            {
+                if (element > maxElement)
+                {
+                    maxElement = element;
+                }
+            }
+            return maxElement;
+        }
+
+        public double FindMinPositiveElement(double[] array)
+        {
+            if (array.Length == 0 || !array.Any(x => x > 0))
+            {
+                throw new ArgumentException("Массив пуст или не содержит положительных элементов");
+            }
+
+            double minPositiveElement = double.MaxValue;
+            foreach (double element in array)
+            {
+                if (element > 0 && element < minPositiveElement)
+                {
+                    minPositiveElement = element;
+                }
+            }
+            return minPositiveElement;
+        }
+
+        public double FindNegativeElement(double[] array)
+        {
+            if (array.Length == 0 || !array.Any(x => x < 0))
+            {
+                throw new ArgumentException("Массив пуст или не содержит отрицательных элементов");
+            }
+
+            double negativeElement = double.MaxValue;
+            foreach (double element in array)
+            {
+                if (element < 0 && element < negativeElement)
+                {
+                    negativeElement = element;
+                }
+            }
+            return negativeElement;
+        }
+    }
+}
---
+++ GenerateInt/Generate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateInt
+{
+    public class Generate
+    {
+        private Random random = new Random();
+        public int GeneratePositive() => random.Next(0, 1001);
+        public int GenerateNegative() => random.Next(-1001, 0);
+        public int GenerateEven()
+        {
+            int value;
+            while (true)
+            {
+                value = random.Next(0, 1001);
+                if (value % 2 == 0)
+                    break;
+            }
+            return value;
+        }
+        public int GenerateOdd()
+        {
+            int value;
+            while (true)
+            {
+                value = random.Next(0, 1001);
+                if (value % 2 == 1)
+                    break;
+            }
+            return value;
+        }
+        public double GenerateNumber(int type)
+        {
+            switch (type)
+            {
+                case 1:
+                    return GeneratePositive();
+                case 2:
+                    return GenerateNegative();
+                case 3:
+                    return GenerateEven();
+                case 4:
+                    return GenerateOdd();
+                default:
+                    throw new ArgumentException("Неверный тип числа");
+            }
+        }
+    }
+}
